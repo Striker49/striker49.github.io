5f60db7bf13e4c1eebd8419066801eb1e63dbf36
Sebastien <roy_sebastien@hotmail.fr>
2025-03-04 18:59:29 -0500

    reverted configs

--- web-ui/vite.config.js
+++ web-ui/vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()], // Add the Vue plugin to handle .vue files
-  root: './web-ui',
   build: {
     outDir: '../dist', // Specify the output folder
     emptyOutDir: true, // Empty outDir before building
