e143685a2374b018de25152dc05339d285f119c4
Sebastien <roy_sebastien@hotmail.fr>
2025-03-04 17:33:32 -0500

    updated package-lock and vite config

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -5,4 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/striker49/',
   plugins: [vue()],
+  root: './web-ui', // Set the root to web-ui folder
+  build: {
+    outDir: '../dist', // Build output will go to the parent directory
+    emptyOutDir: true, // Make sure the output directory is emptied before the build
+  },
 })
