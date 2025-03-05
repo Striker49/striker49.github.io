6f7c504ca184e80f59793d22c045edbebaaff663
Sebastien <roy_sebastien@hotmail.fr>
2025-03-05 11:52:10 -0500

    moved web-ui content outside of folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()], // Add the Vue plugin to handle .vue files
-  root: './web-ui',
+  //root: './web-ui',
   build: {
     outDir: '../dist', // Specify the output folder
     emptyOutDir: true, // Empty outDir before building
