c620e6c8cc39bde9cd340cf7129f72ea915ba851
Sebastien <roy_sebastien@hotmail.fr>
2025-03-05 15:08:14 -0500

    put dist as base path

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     outDir: 'dist', // Specify the output folder
     emptyOutDir: true, // Empty outDir before building
   },
-  base: '/striker49.github.io/striker49/', // Ensure correct base path
+  base: '/dist/', // Ensure correct base path
 });
