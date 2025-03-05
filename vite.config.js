4a8922186b4198175aab3dcf444e78b6c63d88f2
Sebastien <roy_sebastien@hotmail.fr>
2025-03-05 14:46:44 -0500

    added dist to path to vite config files

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()], // Add the Vue plugin to handle .vue files
-  //root: './web-ui',
   build: {
     outDir: '../dist', // Specify the output folder
     emptyOutDir: true, // Empty outDir before building
   },
-  base: '/striker49/', // Ensure correct base path
+  base: '/striker49/dist/', // Ensure correct base path
 });
