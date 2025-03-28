2f66830796740d242a7c92b92992f3319a60922f
Sebastien <roy_sebastien@hotmail.fr>
2025-03-28 01:10:29 -0400

    moved pdf to public folder

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ export default defineComponent({
           <li><a href="#contact">{{ $t("contact") }}</a></li>
         </ul>
         <div style="position: absolute; top: 1rem; right: 1rem;">
-          <a href="/src/assets/CV 2025_FR_EN.pdf.pdf" download="roy_sebastien" id="download" style="position: relative; margin-right: 3rem;"><img src="/src/assets/download-white.png" style="position: relative; height: 1rem; right: 0.5rem;">{{ $t("download") }}</a>
+          <a href="/CV_2025_FR_EN.pdf" download="roy_sebastien" id="download" style="position: relative; margin-right: 3rem;"><img src="/src/assets/download-white.png" style="position: relative; height: 1rem; right: 0.5rem;">{{ $t("download") }}</a>
           <img src="/src/assets/languages.png" alt="Language Icon" id="language-icon" style="position: relative; height: 2rem; right: 10px; top: 0.5rem">
           <label for="language" id="language" style="position: fixed; right: 0"></label>
           <select id="language" name="language" v-model="selectedLanguage" @change="changeLanguage">
@@ -82,7 +82,7 @@ export default defineComponent({
           <li class="project" data-img="/src/assets/no_image.jpg"><h2 class="project">Inception</h2><div><img src="/src/assets/no_image.jpg" alt="Inception image" height="400px"><div class="project-desc">{{ $t("inceptionDesc") }}</div></div></li>
           <li class="project" data-img="/src/assets/cub3d.png" data-gif="/src/assets/cub3d.gif"><h2 class="project">Cub3D</h2><div><img class="static" src="/src/assets/cub3d.png" alt="Cub3D image" height="400px" width="400px"><img class="active" src="/src/assets/cub3d.gif" alt="Cub3D gif" height="400px"><div class="project-desc">{{ $t("cub3dDesc") }}</div></div></li>
         </ul>
-        <a href="/src/assets/CV 2025_FR_EN.pdf.pdf" download="roy_sebastien" style="position: relative; margin-left: -3%; font-size: 2rem; top: 4rem;"><img src="/src/assets/download-white.png" style="position: relative; height: 2rem; right: 0.5rem;">{{ $t("download") }}</a>
+        <a href="/CV_2025_FR_EN.pdf" download="roy_sebastien" style="position: relative; margin-left: -3%; font-size: 2rem; top: 4rem;"><img src="/src/assets/download-white.png" style="position: relative; height: 2rem; right: 0.5rem;">{{ $t("download") }}</a>
       </div>
   </div>
   <footer id="contact">
